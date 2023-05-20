3baf039719d13045b3ab45f5a973920dfdc1e74e
IvanLAyc <cor435253@gmail.com>
2023-05-20 16:02:54 -0300

    sabado20

--- js/compras.js
+++ js/compras.js
@@ -81,8 +81,7 @@ const formaEntrega = () => {
 					<i class="fa-regular fa-circle-xmark"></i>
 					<small>Error message</small>
                 </div>
-                <p> Costo de envio: $350</p>
-            `;
+                <p> Costo de envio: $350</p>`;
             const inputCalle = document.getElementById("calle");
             const inputNumeracion = document.getElementById("numeracion");
             inputCalle.addEventListener("blur", () => {
@@ -119,6 +118,32 @@ const formaEntrega = () => {
             infoExtra.innerHTML = "";
             let nuevaSumaFinalDos = sumaFinal -= costoEnvio;
             checkoutTotal.innerText = nuevaSumaFinalDos;
+            
+            //IFRAME MAPA
+            var iframe = document.createElement("iframe");
+            iframe.onload = function() {
+                var doc = iframe.contentDocument;
+    
+                iframe.contentWindow.showNewMap = function() {
+                var mapContainer =  doc.createElement('div');
+                mapContainer.setAttribute('style',"width: 500px; height: 300px");
+                doc.body.appendChild(mapContainer);
+
+                var mapOptions = {
+                    center: new this.google.maps.LatLng(-35.000009, -58.197645),
+                    zoom: 5,
+                    mapTypeId: this.google.maps.MapTypeId.ROADMAP
+                }
+
+                var map = new this.google.maps.Map(mapContainer,mapOptions);
+            }
+
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&' + 'callback=showNewMap';
+            iframe.contentDocument.getElementsByTagName('head')[0].appendChild(script);
+            };
+            document.body.appendChild(iframe);
         }
     });
 }
